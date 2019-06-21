9d3360ff17ac29f719b4277f4ca4416ee9fb3823
yjhmelody <465402634@qq.com>
2019-06-21 23:48:57 +0800

    add license

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,22 @@
+// Copyright 2019 yjhmelody
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import * as vscode from 'vscode'
 import * as api from './api'
 import * as serverOps from './serverOps'
-import { IDolphindbObject, IDolphindbAttr } from './api';
+import { IDolphindbObject, IDolphindbAttr } from './api'
+
 import { DolphindbContext } from './context';
 
 const DATA_FORM = [
@@ -40,6 +55,7 @@ export class DolphindbEnvProvider implements vscode.TreeDataProvider<vscode.Tree
     getChildren(element?: vscode.TreeItem): vscode.ProviderResult<vscode.TreeItem[]> {
         if (element && element.contextValue === 'variableInfo') {
             // empty
+            return []
         } else if (element && element.contextValue === 'variableType') {
             return this.getVariableInfo(element)
         } else {
