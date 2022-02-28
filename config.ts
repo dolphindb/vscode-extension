abd16f6a71859b09529daf272afa6059fce491f3
Hongfei Shen <hongfei.shen@dolphindb.com>
2022-02-28 17:27:22 +0800

    fix fpd_ext_root

--- config.ts
+++ config.ts
@@ -1,3 +1,5 @@
-export const fpd_ext_root = 'd:/1/ddb/ext/' as const
+import path from 'upath'
+
+export const fpd_ext_root = `${path.normalize(__dirname)}/`
 
 export const fpd_ext_out = `${fpd_ext_root}out/` as const
