4596703b24082f290170a75466100ded917af319
Jiale Gao <jiale.gao@dolphindb.com>
2023-01-17 09:47:43 +0800

    refactor: encapsulation runDA into function

--- debugAdapter/index.ts
+++ debugAdapter/index.ts
@@ -18,8 +18,6 @@ const fsAccessor: FileAccessor = {
 	}
 };
 
-console.log('Debug adapter started');
-
 /*
  * When the debug adapter is run as an external process,
  * normally the helper function DebugSession.run(...) takes care of everything:
@@ -29,34 +27,38 @@ console.log('Debug adapter started');
  * but here the helper is not flexible enough to deal with a debug session constructors with a parameter.
  * So for now we copied and modified the helper:
  */
-
-// first parse command line arguments to see whether the debug adapter should run as a server
-let port = 0;
-const args = process.argv.slice(2);
-args.forEach(function (val, index, array) {
-	const portMatch = /^--server=(\d{4,5})$/.exec(val);
-	if (portMatch) {
-		port = parseInt(portMatch[1], 10);
-	}
-});
-
-if (port > 0) {
-	// start a server that creates a new session for every connection request
-	console.error(`waiting for debug protocol on port ${port}`);
-	Net.createServer((socket) => {
-		console.error('>> accepted connection from client');
-		socket.on('end', () => {
-			console.error('>> client connection closed\n');
-		});
-		const session = new MockDebugSession(fsAccessor);
-		session.setRunAsServer(true);
-		session.start(socket, socket);
-	}).listen(port);
-} else {
-	// start a single session that communicates via stdin/stdout
-	const session = new MockDebugSession(fsAccessor);
-	process.on('SIGTERM', () => {
-		session.shutdown();
+function runDebugAdapter(debugSession: typeof MockDebugSession) {
+	console.log('Debug adapter started');
+	// first parse command line arguments to see whether the debug adapter should run as a server
+	let port = 0;
+	const args = process.argv.slice(2);
+	args.forEach((val) => {
+		const portMatch = /^--server=(\d{4,5})$/.exec(val);
+		if (portMatch) {
+			port = parseInt(portMatch[1], 10);
+		}
 	});
-	session.start(process.stdin, process.stdout);
+
+	if (port > 0) {
+		// start a server that creates a new session for every connection request
+		console.error(`waiting for debug protocol on port ${port}`);
+		Net.createServer((socket) => {
+			console.error('>> accepted connection from client');
+			socket.on('end', () => {
+				console.error('>> client connection closed\n');
+			});
+			const session = new debugSession(fsAccessor);
+			session.setRunAsServer(true);
+			session.start(socket, socket);
+		}).listen(port);
+	} else {
+		// start a single session that communicates via stdin/stdout
+		const session = new debugSession(fsAccessor);
+		process.on('SIGTERM', () => {
+			session.shutdown();
+		});
+		session.start(process.stdin, process.stdout);
+	}
 }
+
+runDebugAdapter(MockDebugSession);
