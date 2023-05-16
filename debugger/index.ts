6c27590b0aa3e40c942727b0a7099b414a6585b7
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-05-16 17:12:28 +0800

    deps;

--- debugger/index.ts
+++ debugger/index.ts
@@ -1,11 +1,9 @@
-/*
-    整个Debug的启动流程是这样的：
-    1. ddb-vsc插件在初始化(activate)时会调用activate.ts中的 `activateDebug` 注册一个debug configuration provider
-    2. 用户启动debug会话时，这个provider会首先从插件处获取当前连接的数据库url，动态修改用户launch配置
-    3. 这个文件下的 `runDebugAdapter` 被调用，创建一个debugSession(adapter)
-    4. 在debugSession中，vsc会依次调用initialize、launch、setBreakpoints、configurationDone等，
-        这个过程中会连接服务端，并把用户脚本和断点情况发送过去，服务端验证断点返回，执行到第一个断点处，vsc等待用户下一步操作
-*/
+// 整个Debug的启动流程是这样的：
+// 1. ddb-vsc插件在初始化(activate)时会调用activate.ts中的 `activateDebug` 注册一个debug configuration provider
+// 2. 用户启动debug会话时，这个provider会首先从插件处获取当前连接的数据库url，动态修改用户launch配置
+// 3. 这个文件下的 `runDebugAdapter` 被调用，创建一个debugSession(adapter)
+// 4. 在 debugSession 中，vsc 会依次调用 initialize 、 launch 、 setBreakpoints 、 configurationDone 等，
+//     这个过程中会连接服务端，并把用户脚本和断点情况发送过去，服务端验证断点返回，执行到第一个断点处，vsc等待用户下一步操作
 import { DdbDebugSession } from './adapter.js'
 import * as Net from 'net'
 
