0f7c5def6b70cdfde3741f1d64b4494be85fd209
Tianchen Fu <tianchen.fu@dolphindb.com>
2024-06-05 09:04:04 +0800

    enhance build script

--- server/rollup.config.mjs
+++ server/rollup.config.mjs
@@ -8,23 +8,23 @@ const isProd = mode === 'prod';
 export default {
   input: `./src/server.ts`,
   output: [
-    {
-      file: './build/cjs/server.js',
-      exports: 'named',
-      format: 'cjs',
-      sourcemap: !isProd
-    },
+    // {
+    //   file: './build/cjs/server.js',
+    //   exports: 'named',
+    //   format: 'cjs',
+    //   sourcemap: !isProd
+    // },
     {
       file: './build/server.js',
       format: 'es',
       sourcemap: !isProd
     },
-    {
-      file: 'build/server.global.js',
-      name: 'DDBLS',
-      format: 'iife',
-      sourcemap: !isProd
-    },
+    // {
+    //   file: 'build/server.global.js',
+    //   name: 'DDBLS',
+    //   format: 'iife',
+    //   sourcemap: !isProd
+    // },
   ],
   plugins: [resolve(),commonjs(),typescript({
     useTsconfigDeclarationDir: true,
