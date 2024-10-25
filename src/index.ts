757ac4c7bfecbe95a9cba1f5ba005b29b41b66bf
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-10-25 15:43:11 +0800

    自动添加设置;

--- src/index.ts
+++ src/index.ts
@@ -35,22 +35,11 @@ import { register_terminal_link_provider } from './terminal.ts'
 import { connector, register_connector } from './connector.ts'
 import { register_variables } from './variables.ts'
 import { register_databases } from './databases.ts'
+import { register_settings } from './settings.ts'
 
 
 export type DdbMessageItem = MessageItem & { action?: () => void | Promise<void> }
 
-type TokenColorCustomizations = {
-    textMateRules?: Array<TextMateRule>
-}
-
-type TextMateRule = {
-    scope: string | string[]
-    settings: {
-        foreground?: string
-        background?: string
-        fontStyle?: string
-    }
-}
 
 if (util.inspect.styles.number !== 'green')
     set_inspect_options()
@@ -153,35 +142,8 @@ export async function activate (ctx: ExtensionContext) {
         }
     }))
     
-    /** 添加 decorator 颜色 */
-    const config = workspace.getConfiguration('editor')
+    await register_settings()
     
-    // 获取当前的 tokenColorCustomizations，不存在则创建
-    const token_color_customizations: TokenColorCustomizations = config.get<TokenColorCustomizations>('tokenColorCustomizations') || { }
-    const textmate_rules: TextMateRule[] = token_color_customizations.textMateRules || [ ]
-    
-    // 添加新的规则
-    const new_rule: TextMateRule = {
-        scope: 'meta.decorator.dolphindb',
-        settings: {
-            foreground: '#aa6f00'
-        }
-    }
-    
-    // 检查是否已经存在该规则，避免重复添加
-    const rule_exists = textmate_rules.some((rule: TextMateRule) => rule.scope === 'meta.decorator.dolphindb')
-    if (!rule_exists) {
-        textmate_rules.push(new_rule)
-        token_color_customizations.textMateRules = textmate_rules
-        
-        // 更新配置
-        config.update('tokenColorCustomizations', token_color_customizations, ConfigurationTarget.Global)
-            .then(() => {
-                // 更新规则成功，什么也不做
-            }, _ => {
-                window.showErrorMessage('Failed to update token color customization')
-            })
-    }
     console.log(t('DolphinDB 插件初始化成功'))
 }
 
