1923486f8b7a77bab1adce3870d868922a76feb4
Tianchen Fu <tianchen.fu@dolphindb.com>
2024-12-02 17:44:11 +0800

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
 import util from 'util'
 
+import * as path from 'path'
+
 import {
     window,
     workspace,
-
+    
     commands,
-
+    
     extensions, ExtensionMode,
-
+    
     type ExtensionContext,
-
+    
     type ProviderResult,
-
+    
     ConfigurationTarget,
-
+    
     debug, type DebugConfiguration,
-
+    
     type MessageItem
 } from 'vscode'
 
 import 'xshell/polyfill.browser.js'
 import { set_inspect_options } from 'xshell'
 
+
+import {
+    LanguageClient,
+    type LanguageClientOptions,
+    type ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node.js'
 
 import { t } from '../i18n/index.ts'
 
@@ -36,16 +45,8 @@ import { connector, register_connector } from './connector.ts'
 import { register_variables } from './variables.ts'
 import { register_databases } from './databases.ts'
 import { register_settings } from './settings.ts'
-import * as path from 'path';
 
-import {
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-    TransportKind
-} from 'vscode-languageclient/node.js';
-
-let client: LanguageClient;
+let client: LanguageClient
 
 
 export type DdbMessageItem = MessageItem & { action?: () => void | Promise<void> }
@@ -64,9 +65,9 @@ export let extctx: ExtensionContext
 export let dev = false
 
 
-export async function activate(ctx: ExtensionContext) {
+export async function activate (ctx: ExtensionContext) {
     extctx = ctx
-
+    
     dev = ctx.extensionMode === ExtensionMode.Development
     console.log(t('dolphindb 插件运行在{{mode}}模式下，版本为 {{version}}', {
         mode: dev ? t('开发') : t('生产'),
@@ -77,23 +78,23 @@ export async function activate(ctx: ExtensionContext) {
     // 命令注册
     for (const func of ddb_commands)
         ctx.subscriptions.push(commands.registerCommand(`dolphindb.${func.name}`, func))
-
-
+    
+    
     // 在 package.json 中设置 configurationDefaults 不生效，只好通过 api 修改
     let config_window = workspace.getConfiguration('window')
     if (config_window.get<'native' | 'custom'>('dialogStyle') === 'native')
         await config_window.update('dialogStyle', 'custom', ConfigurationTarget.Global)
-
-
+    
+    
     register_connector()
     register_variables()
     register_databases()
-
+    
     window.onDidChangeActiveTextEditor(() => {
-        connector.change_language_mode()
+        connector.change_language_mode()    
     })
-
-
+    
+    
     formatter.init()
     statbar.init()
     
@@ -102,33 +103,33 @@ export async function activate(ctx: ExtensionContext) {
         formatter.on_config_change(event)
         connector.on_config_change(event)
     })
-
+    
     register_terminal_link_provider()
     
     await load_docs()
     register_docs(ctx)
-
-
+    
+    
     dataview.register()
-
-
+    
+    
     ctx.subscriptions.push(debug.registerDebugConfigurationProvider('dolphindb', {
-        resolveDebugConfiguration(folder, config, token): ProviderResult<DebugConfiguration> {
+        resolveDebugConfiguration (folder, config, token): ProviderResult<DebugConfiguration> {
             // 默认使用当前插件连接的 server 作为 debugger
             const { connection: { url, options: { python, password, username, autologin } } } = connector
-
+            
             if (python) {
                 window.showWarningMessage(t('python parser 暂不支持调试功能'))
                 return
             }
-
+            
             const languageId = window.activeTextEditor?.document.languageId
-
+            
             // if launch.json is missing or empty
             if (
-                !config.type &&
-                !config.request &&
-                !config.name &&
+                !config.type && 
+                !config.request && 
+                !config.name && 
                 languageId === 'dolphindb'
             ) {
                 config.type = 'dolphindb'
@@ -136,34 +137,32 @@ export async function activate(ctx: ExtensionContext) {
                 config.name = t('调试当前 DolphinDB 脚本文件')
                 config.program = '${file}'
             }
-
+            
             config.url ??= url
             config.username ??= username
             config.password ??= password
             config.autologin = autologin
-
+            
             // 并不能在这里限制非. dos 文件被选中作为 debugee，此时 ${file} 还未被解析成绝对路径
             if (!config.program) {
                 window.showInformationMessage(t('调试配置 program 字段为空，请指定为待调试的脚本路径'))
                 return
             }
-
+            
             return config
         }
     }))
     
     await register_settings()
     
-
-    /**
-     * 初始化 Language Server
-     */
+    
+    /** 初始化 Language Server */
     // The server is implemented in node
-    let serverModule = ctx.asAbsolutePath(path.join('server', 'server.js'));
+    let serverModule = ctx.asAbsolutePath(path.join('server', 'server.js'))
     // The debug options for the server
     // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
+    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] }
+    
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
     let serverOptions: ServerOptions = {
@@ -173,8 +172,8 @@ export async function activate(ctx: ExtensionContext) {
             transport: TransportKind.ipc,
             options: debugOptions
         }
-    };
-
+    }
+    
     // Options to control the language client
     let clientOptions: LanguageClientOptions = {
         // Register the server for plain text documents
@@ -186,31 +185,29 @@ export async function activate(ctx: ExtensionContext) {
         initializationOptions: {
             configuration: workspace.getConfiguration('dolphindb')
         }
-    };
-
+    }
+    
     // Create the language client and start the client.
     client = new LanguageClient(
         'ddbls',
         'Dolphin DB Language Server',
         serverOptions,
         clientOptions
-    );
-
+    )
+    
     // Start the client. This will also launch the server
-    client.start();
-
+    client.start()
+    
     console.log(t('DolphinDB 插件初始化成功'))
 }
 
 
-export function deactivate(ctx: ExtensionContext) {
+export async function deactivate (ctx: ExtensionContext) {
     server?.stop()
-
-    /**
-     * 停止 Language Server 连接
-     */
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
+    
+    /** 停止 Language Server 连接 */
+    if (!client) 
+        return undefined
+    
+    return client.stop()
 }
