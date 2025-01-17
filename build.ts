8fd99a7b26a2040e641c85ab5891a9f2982c3adb
Tianchen Fu <tianchen.fu@dolphindb.com>
2025-01-17 15:39:33 +0800

    fix: build

--- build.ts
+++ build.ts
@@ -1,6 +1,18 @@
+import * as cp from 'child_process'
+
 import { builder } from './builder.ts'
 
 
 await builder.build(true)
-
+await new Promise(resolve => {
+    cp.exec('pnpm run build-languageserver', (error, stdout, stderr) => {
+        if (error) {
+            console.error(error)
+            resolve(false)
+        } else {
+            console.log(stdout)
+            resolve(true)
+        }
+    })
+})
 await builder.close()
