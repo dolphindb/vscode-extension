e006b3b516ae6bba65c0b963a7962c69f714aeba
Tianchen Fu <tianchen.fu@dolphindb.com>
2025-05-06 14:59:01 +0800

    修复连接方法中的调试会话检查，确保仅在特定调试类型下显示警告

--- src/connector.ts
+++ src/connector.ts
@@ -72,7 +72,7 @@ export class DdbConnector implements TreeDataProvider<TreeItem> {
     
     /** 执行连接操作后，如果超过 1s 还未完成，则显示进度 */
     async connect (connection: DdbConnection) {
-        if (debug.activeDebugSession) {
+        if (debug.activeDebugSession && debug.activeDebugSession.type === 'dolphindb') {
             window.showWarningMessage(t('当前有 debug 进程进行中，无法切换连接'))
             return
         }
