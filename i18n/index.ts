f4a1a1fa5a4bb14e73f71650082f48a05c336250
Hongfei Shen <hongfei.shen@dolphindb.com>
2026-03-13 11:33:41 +0800

    .

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,5 +1,3 @@
-import process from 'node:process'
-
 import { I18N, type Language } from 'xshell/i18n/index.js'
 
 import _dict from './dict.json' with { type: 'json' }
@@ -9,7 +7,7 @@ import _dict from './dict.json' with { type: 'json' }
 // const i18n = new I18N(_dict, 'en')
 const i18n = new I18N(
     _dict,
-    process?.env?.VSCODE_NLS_CONFIG ?
+    globalThis.process?.env?.VSCODE_NLS_CONFIG ?
         JSON.parse(
             process.env.VSCODE_NLS_CONFIG || '{ "locale": "zh" }'
         ).locale.slice(0, 2) as Language
